ead33d3696088c278e0055a328a2253e65ab4929
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2023-07-02 23:10:36 +0200

    cleanup for deploy 0.4 - config file problems

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+  readonly VITE_NODE_ENV: 'development' | 'production' | 'test'
+  readonly VITE_PRODUCTION_ENDPOINT: string
+  readonly VITE_PRODUCTION_IOENDPOINT: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+export { ImportMeta, ImportMetaEnv }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,80 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-const isProduction = process.env.VITE_NODE_ENV === 'production'
-const proxyTarget = isProduction
-  ? process.env.VITE_PRODUCTION_ENDPOINT
-  : 'http://localhost:5000'
-const ioTarget = isProduction
-  ? process.env.VITE_PRODUCTION_IOENDPOINT
-  : 'ws://localhost:5174'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      // '/streamers': 'http://localhost:5000',
-      '/streamers': proxyTarget,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
-      '/socket.io': {
-        // target: 'ws://localhost:5174',
-        target: ioTarget,
-        ws: true
+  const isProduction: boolean = process.env.VITE_NODE_ENV === 'production'
+  const proxyTarget: string = isProduction
+    ? process.env.VITE_PRODUCTION_ENDPOINT
+    : 'http://localhost:5000'
+  const ioTarget: string = isProduction
+    ? process.env.VITE_PRODUCTION_IOENDPOINT
+    : 'ws://localhost:5174'
+  return defineConfig({
+    plugins: [react()],
+
+    server: {
+      proxy: {
+        '/streamers': proxyTarget,
+        '/socket.io': {
+          target: ioTarget,
+          ws: true
+        }
       }
     }
-  }
-})
+  })
+}
+
+// export default ({ mode }) => {
+//   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+// const isProduction: boolean = import.meta.env.VITE_NODE_ENV === 'production'
+// const proxyTarget: string = isProduction
+//   ? import.meta.env.VITE_PRODUCTION_ENDPOINT
+//   : 'http://localhost:5000'
+// const ioTarget: string = isProduction
+//   ? import.meta.env.VITE_PRODUCTION_IOENDPOINT
+//   : 'ws://localhost:5174'
+
+//   return defineConfig({
+//     plugins: [react()],
+//     server: {
+//       proxy: {
+//         '/streamers': proxyTarget,
+//         '/socket.io': {
+//           target: ioTarget,
+//           ws: true
+//         }
+//       }
+//     }
+//   })
+// }
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// const isProduction = process.env.VITE_NODE_ENV === 'production'
+// const proxyTarget = isProduction
+//   ? process.env.VITE_PRODUCTION_ENDPOINT
+//   : 'http://localhost:5000'
+// const ioTarget = isProduction
+//   ? process.env.VITE_PRODUCTION_IOENDPOINT
+//   : 'ws://localhost:5174'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       // '/streamers': 'http://localhost:5000',
+//       '/streamers': proxyTarget,
+
+//       '/socket.io': {
+//         // target: 'ws://localhost:5174',
+//         target: ioTarget,
+//         ws: true
+//       }
+//     }
+//   }
+// })
