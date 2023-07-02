83349fbe557b3f4e162e612bae3845b42b85e6f4
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2023-07-02 22:18:58 +0200

    cleanup for deploy 0.3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const isProduction = process.env.VITE_NODE_ENV === 'production'
+const proxyTarget = isProduction
+  ? 'https://streamerratebe.onrender.com/'
+  : 'http://localhost:5000'
+const ioTarget = isProduction
+  ? 'streamerrate.netlify.app/'
+  : 'ws://localhost:5174'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/streamers': 'http://localhost:5000',
+      // '/streamers': 'http://localhost:5000',
+      '/streamers': proxyTarget,
 
-      // Proxying websockets or socket.io: ws://localhost:5173/socket.io -> ws://localhost:5174/socket.io
       '/socket.io': {
-        target: 'ws://localhost:5174',
+        // target: 'ws://localhost:5174',
+        target: ioTarget,
         ws: true
       }
     }
